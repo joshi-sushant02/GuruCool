04c8cd664f0e418db37a53692f288e285f1ecec3
Fresher-02 <susjos7875@gmail.com>
2022-08-25 16:40:21 +0530

    initial commit

---
+++ GuruCool/urls.py
@@ -0,0 +1,22 @@
+"""GuruCool URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+"""
+# Django imports
+from django.urls import re_path as url 
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    # Examples:
+    # url(r'^blog/', include('blog.urls', namespace='blog')),
+
+    # provide the most basic login/logout functionality
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='core/login.html'),
+        name='core_login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='core_logout'),
+
+    # enable the admin interface
+    url(r'^admin/', admin.site.urls),
+]
---
+++ README.md
@@ -0,0 +1,5 @@
+# GuruCool
+A Virtual Classroom.<br>
+Full Stack WebSite<br>
+Front End- Html Css Js<br>
+BackEnd- Django<br>
